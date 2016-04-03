55949cd525e717b74b320ed629ed9c9ac9e11e9b
Bryan Hanson <hanson@depauw.edu>
2016-04-02 20:34:38 -0400

    new import functions

--- R/files2SpectraObject2.R
+++ R/files2SpectraObject2.R
@@ -1,9 +1,9 @@
-files2SpectraObject <-
+files2SpectraObject2 <-
 function(gr.crit = NULL, gr.cols = c("auto"),
 	freq.unit = "no frequency unit provided",
 	int.unit = "no intensity unit provided",
 	descrip = "no description provided",
-	format = "csv",
+	fileExt = "\\.(csv|CSV)$",
 	out.file = "mydata", debug = FALSE, ...) {
 		
 # Function to Read & Prep Spectroscopic Data
@@ -11,15 +11,13 @@ function(gr.crit = NULL, gr.cols = c("auto"),
 # Part of the ChemoSpec package
 # Bryan Hanson, DePauw University, Aug 2009
 
-# This function acts on all files in the working directory
+# This function acts on all files in the working directory with the specified extension
 
-# CSV files should have freq in column 1, absorbance/intensity in column 2.
-# There should be no column labels.
+# Files should have freq in column 1, absorbance/intensity in column 2.
+# There may or may not be a header (default is FALSE for read.table)
 
 # DX files can be parsed (see readJDX for limitations)
 
-	message("The behavior of files2SpectraObject will be changing in summer 2016.  In the meantime, files2SpectraObject2 gives a more flexible interface, which will become the new standard after testing.")
-	
 	if (!requireNamespace("R.utils", quietly = TRUE)) {
 		stop("You need to install package R.utils to use this function")
 		}
@@ -28,41 +26,38 @@ function(gr.crit = NULL, gr.cols = c("auto"),
 
 	# First set up some common stuff
 	
-	if ((format == "csv") | (format == "csv2")) pat = "\\.(csv|CSV)$"
-	if (format == "dx") pat = "\\.(dx|DX|jdx|JDX)$"
-	files <- list.files(pattern = pat)
-	if ((format == "csv") | (format == "csv2")) {
-		files.noext <- substr(basename(files), 1, nchar(basename(files)) - 4)
-		}
-	if (format == "dx") files.noext <- substr(basename(files), 1, nchar(basename(files)) - 3)		
+	# if ((format == "csv") | (format == "csv2")) pat = "\\.(csv|CSV)$"
+	# if (format == "dx") pat = "\\.(dx|DX|jdx|JDX)$"
+	DX = FALSE
+	if (grepl("(dx|DX|jdx|JDX)", fileExt)) DX <- TRUE
+	
+	files <- list.files(pattern = fileExt)
+	files.noext <- tools::file_path_sans_ext(files)
+
 	spectra <- list() # OK to initialize a list this way
 	spectra$names <- files.noext
-			
-	
+
 	if (debug) message("\nfiles2SpectraObject is checking the first file")
-	if (format == "csv") temp <- read.csv(files[1], header = FALSE, ...)
-	if (format == "csv2") temp <- read.csv2(files[1], header = FALSE, ...)
-	if (format == "dx") {
-		temp <- readJDX(file = files[1], debug = debug)
-		}
+	if (!DX) temp <- read.table(files[1], ...)
+	if (DX) temp <- readJDX(file = files[1], debug = debug)
 
 	spectra$freq <- temp[,1]
 	if (class(spectra$freq) == "integer") {
-		message("\nConverting integer frequency values to numeric")
+		if (debug) message("\nConverting integer frequency values to numeric")
 		spectra$freq <- as.numeric(spectra$freq)
 		}
 	
-	spectra$data <- matrix(data = NA, nrow = length(files), ncol = length(spectra$freq))
+	spectra$data <- matrix(data = NA_real_, nrow = length(files), ncol = length(spectra$freq))
 	
-	# loop over all files (you have to read the whole file then grab
+	# Loop over all files (you have to read the whole file then grab
 	# just the part you want, i.e. the 2nd column)
 
 	if (debug) message("\nfiles2SpectraObject will now import your files")
+	
 	for (i in 1:length(files)) {
 		if (debug) cat("Importing file: ", files[i], "\n")
-		if (format == "csv") temp <- read.csv(files[i], header = FALSE, ...)
-		if (format == "csv2") temp <- read.csv2(files[i], header = FALSE, ...)
-		if (format == "dx") temp <- readJDX(files[i], debug = debug, ...)
+		if (!DX) temp <- read.table(files[i], ...)
+		if (DX) temp <- readJDX(files[i], debug = debug, ...)
 		spectra$data[i,] <- temp[,2]
 		}
 
