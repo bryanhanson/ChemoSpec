524dbdf32b8e6fb7892b24182a29f0520a055465
Bryan <hanson@depauw.edu>
2021-05-12 18:05:57 -0700

    improve onAttach msg

--- R/onAttach.R
+++ R/onAttach.R
@@ -4,6 +4,6 @@
 .onAttach <- function(libname, pkgname) {
   options(ChemoSpecGraphics = "base")
 
-  packageStartupMessage("The ChemoSpec graphics option is set to 'base'")
+  packageStartupMessage("\nThe ChemoSpec graphics option is set to 'base'")
   packageStartupMessage("To change it, do\n\toptions(ChemoSpecGraphics = 'option'),\n\twhere 'option' is one of 'base' or 'ggplot2'")
 }
