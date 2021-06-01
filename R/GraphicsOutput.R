5997adefbe0ab592d61a00749b44ebcc15907eed
Tejasvi Gupta <tejasvigupta723@gmail.com>
2021-06-01 21:01:18 +0530

    Added the NULL statement and did some minor changes

--- R/GraphicsOutput.R
+++ R/GraphicsOutput.R
@@ -1,18 +1,13 @@
 #'
 #' Graphic options in ChemoSpec
 #'
-#' In \code{ChemoSpec}, the user may use any graphic mode from 'base' and 'ggplo2' for plotting.
-#' The current graphic mode can be found using \code{\link[ChemoSpec]{chkGrafOpt}}.
+#' In \code{ChemoSpec}, the user may use any graphic mode from 'base' and 'ggplot2' for plotting.
+#' The graphic mode can be found using \code{chkGrafOpt()}.
 #' Initially the graphic option is set to 'base'.
-#' The graphic options can be changed by doing \code{(ChemoSpecGraphics = 'option')}, where 'option' is one of 'base' or 'ggplot2'
+#' The graphic options can be changed by doing \code{options(ChemoSpecGraphics = 'option')}, where 'option' is one of 'base' or 'ggplot2'
 #' 
-#'  
+#' @docType data
 #' 
-#'
-#' @return
-#' @export
+#' @name GraphicsOutput
 #' 
-#' @export
-#'
-#' @examples
-#' 
+NULL
