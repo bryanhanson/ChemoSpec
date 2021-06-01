d30ba59677d63c5b7fa39c64b481516365c0507a
Tejasvi Gupta <tejasvigupta723@gmail.com>
2021-06-01 13:31:27 +0530

    Added GraphicsOutput file

---
+++ R/GraphicsOutput.R
@@ -0,0 +1,18 @@
+#'
+#' Graphic options in ChemoSpec
+#'
+#' In \code{ChemoSpec}, the user may use any graphic mode from 'base' and 'ggplo2' for plotting.
+#' The current graphic mode can be found using \code{\link[ChemoSpec]{chkGrafOpt}}.
+#' Initially the graphic option is set to 'base'.
+#' The graphic options can be changed by doing \code{(ChemoSpecGraphics = 'option')}, where 'option' is one of 'base' or 'ggplot2'
+#' 
+#'  
+#' 
+#'
+#' @return
+#' @export
+#' 
+#' @export
+#'
+#' @examples
+#' 
