bb0b77156cc47b87e0a85cb81f8fdbb3bfb16fc6
Tejasvi Gupta <tejasvigupta723@gmail.com>
2021-06-07 23:05:37 +0530

    Added the test cases for when the graphic mode is unset or an invalid graphic mode has been selected

--- inst/tinytest/test_chkGraphicsOpt.R
+++ inst/tinytest/test_chkGraphicsOpt.R
@@ -1,9 +1,19 @@
 
 ### Unit tests for chkGraphicsOpt in ChemoSpec
 
-options(ChemoSpecGraphics = 'base')
-expect_equal(chkGraphicsOpt() ,"base")
+# check for 'base'
+options(ChemoSpecGraphics = "base")
+expect_equal(chkGraphicsOpt(), "base")
 
-options(ChemoSpecGraphics = 'ggplot2')
-expect_equal(chkGraphicsOpt() ,"ggplot2")
+# check for 'ggplot2'
+options(ChemoSpecGraphics = "ggplot2")
+expect_equal(chkGraphicsOpt(), "ggplot2")
 
+
+# Check for empty values
+options(ChemoSpecGraphics = "")
+expect_equal(chkGraphicsOpt(), "base")
+
+# check for invalid value
+options(ChemoSpecGraphics = "xyz")
+expect_equal(chkGraphicsOpt(), "base")
