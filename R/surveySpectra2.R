54727df1d18777749f0d1e5b418ffc734589acb6
Bryan <hanson@depauw.edu>
2021-07-18 15:38:04 -0700

    code review surveySpectra2

--- R/surveySpectra2.R
+++ R/surveySpectra2.R
@@ -4,7 +4,8 @@
 #'
 #' @describeIn surveySpectra Spectral survey emphasizing variation among spectra.
 #'
-surveySpectra2 <- function(spectra, method = c("sd", "sem", "sem95", "mad", "iqr"),
+surveySpectra2 <- function(spectra,
+                           method = c("sd", "sem", "sem95", "mad", "iqr"),
                            lab.pos = 0.9 * max(spectra$freq), ...) {
   .chkArgs(mode = 11L)
   chkSpectra(spectra)
@@ -42,7 +43,9 @@ surveySpectra2 <- function(spectra, method = c("sd", "sem", "sem95", "mad", "iqr
     y <- y[, 3] - y[, 2]
     lab <- "sem95"
   }
+
   go <- chkGraphicsOpt()
+
   if (go == "base") {
 
     # Now set up the plot and plot it!
@@ -51,51 +54,43 @@ surveySpectra2 <- function(spectra, method = c("sd", "sem", "sem95", "mad", "iqr
     ymax <- max(M)
 
     # Offset the summary stat below everything else, with a small gap for aesthetics
-
     off1 <- diff(range(y))
     off2 <- min(M) - 0.05 * diff(range(M))
-    ymin <- off2 - off1
     off3 <- abs(min(M)) + abs(max(y)) + abs(0.05 * diff(range(M)))
+    off4 <- min(M[nrow(M), ]) - off3 + 0.5 * diff(range(y))
 
+    ymin <- off2 - off1
     plot(x, M[1, ], type = "n", ylim = c(ymin, ymax), xlab = spectra$unit[1], ylab = "Centered Spectra", ...)
     for (i in 1:(nrow(M) - 1)) {
       lines(x, M[i, ], col = spectra$colors[i])
     }
     lines(x, M[nrow(M), ] - off3)
-    off4 <- min(M[nrow(M), ]) - off3 + 0.5 * diff(range(y))
     text(x = lab.pos, y = off4, labels = lab, cex = 1.2, adj = c(0.5, 1))
   }
+
   if (go == "ggplot2") {
     value <- variable <- Frequency <- NULL # satisfy CRAN check engine
-
-
-
-    # M <- rbind(M, y)
     ymax <- max(M)
 
-    # New small dataframe for plotting the different geom_line()
-    df1 <- data.frame(x, y)
-
+    DF_spread <- data.frame(x, y) # holds measure of spread, e.g sd, seX etc
 
     # Offset the summary stat below everything else, with a small gap for aesthetics
-
     off1 <- diff(range(y))
     off2 <- min(M) - 0.05 * diff(range(M))
-    ymin <- off2 - off1
     off3 <- abs(min(M)) + abs(max(y)) + abs(0.05 * diff(range(M)))
+    off4 <- min(DF_spread$y) - off3 + 0.5 * diff(range(y)) # offset for label
 
-    df <- data.frame(x)
-    for (i in 1:length(spectra$names))
-    {
+    DF_centered <- data.frame(x)
+    for (i in 1:length(spectra$names)) {
       spec <- M[i, ]
-      df <- cbind(df, spec)
+      DF_centered <- cbind(DF_centered, spec)
     }
-    names(df) <- c("Frequency", spectra$names)
+    names(DF_centered) <- c("Frequency", spectra$names)
+    molten <- reshape2::melt(DF_centered, id = c("Frequency"))
 
-    molten.data <- reshape2::melt(df, id = c("Frequency"))
     p <- ggplot() +
       geom_line(
-        data = molten.data,
+        data = molten,
         aes(x = Frequency, y = value, group = variable, color = variable)
       ) +
       scale_color_manual(name = "Key", values = spectra$colors) +
@@ -103,13 +98,7 @@ surveySpectra2 <- function(spectra, method = c("sd", "sem", "sem95", "mad", "iqr
       theme(legend.position = "none") +
       theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
       labs(x = spectra$unit[1], y = "centered Spectra")
-    p <- p + geom_line(data = df1, aes(x = x, y = y - off3))
-
-    off4 <- min(df1$y) - off3 + 0.5 * diff(range(y))
-    keys <- grobTree(textGrob("method",
-      x = lab.pos, y = off4, hjust = 0,
-      gp = gpar(col = "black", fontsize = 12)
-    ))
+    p <- p + geom_line(data = DF_spread, aes(x = x, y = y - off3))
 
     p <- p + annotate(
       geom = "text", x = lab.pos, y = off4, label = method,
