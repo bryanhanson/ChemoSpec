9f02185422e6d9c221ff64d7ef8b3bfa30f3d6f3
Bryan <hanson@depauw.edu>
2021-08-26 19:09:18 -0700

    review/style aovPCAscores

--- R/aovPCAscores.R
+++ R/aovPCAscores.R
@@ -72,12 +72,15 @@ aovPCAscores <- function(spectra, so, submat = 1, ellipse = "none", tol = "none"
   }
 
   if (go == "ggplot2") {
+    chkReqGraphicsPkgs("ggplot2")
     so <- so[[submat]] # need to force evaluation for some reason (do.call is downstream)
     p <- plotScores(spectra, so, ellipse = ellipse, tol = tol, use.sym = use.sym, leg.loc = leg.loc, ...)
     return(p)
   }
   
   if (go == "plotly") {
+    chkReqGraphicsPkgs("ggplot2")
+    chkReqGraphicsPkgs("plotly")
     so <- so[[submat]] # need to force evaluation for some reason (do.call is downstream)
     p <- plotScores(spectra, so, ellipse = ellipse, tol = tol, use.sym = use.sym, leg.loc = leg.loc, ...)
     return(p)
